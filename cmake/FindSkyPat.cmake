b8732aeb62e451b76c2549bf3002931552969a11
LuisHsu <luishsuplus@gmail.com>
2018-08-23 04:00:34 +0800

    修正 SKYPAT_LIBRARY_DIR 和 SKYPAT_INCLUDE_DIR

--- cmake/FindSkyPat.cmake
+++ cmake/FindSkyPat.cmake
@@ -1,10 +1,11 @@
-find_library(SKYPAT_LIBRARIES
-    NAMES skypat
-    HINTS ${SKYPAT_PATH}
-)
+if(NOT SKYPAT_LIBRARY_DIR)
+    find_library(SKYPAT_LIBRARIES
+        NAMES skypat
+    )
+    get_filename_component(SKYPAT_LIBRARY_DIR ${SKYPAT_LIBRARIES} DIRECTORY)
+endif(NOT SKYPAT_LIBRARY_DIR)
 find_path(SKYPAT_INCLUDE_DIR
     NAMES skypat/skypat.h
-    HINTS ${SKYPAT_PATH}
 )
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SKYPAT REQUIRED_VARS SKYPAT_INCLUDE_DIR SKYPAT_LIBRARIES)
+find_package_handle_standard_args(SKYPAT REQUIRED_VARS SKYPAT_INCLUDE_DIR SKYPAT_LIBRARY_DIR)
