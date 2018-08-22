13559d119c69339bbc83631f9716bc4cd94866af
LuisHsu <luishsuplus@gmail.com>
2018-08-23 03:30:24 +0800

    讓 FindSkyPat 能從 SKYPAT_PATH 開始找

--- cmake/FindSkyPat.cmake
+++ cmake/FindSkyPat.cmake
@@ -1,10 +1,11 @@
+if(SKYPAT_PATH)
+set(CMAKE_FIND_ROOT_PATH ${SKYPAT_PATH} ${CMAKE_FIND_ROOT_PATH})
+endif(SKYPAT_PATH)
 find_library(SKYPAT_LIBRARIES
     NAMES skypat
-    HINTS ${SKYPAT_PATH}
 )
 find_path(SKYPAT_INCLUDE_DIR
     NAMES skypat/skypat.h
-    HINTS ${SKYPAT_PATH}
 )
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SKYPAT DEFAULT_MSG SKYPAT_INCLUDE_DIR SKYPAT_LIBRARIES)
+find_package_handle_standard_args(SKYPAT REQUIRED_VARS SKYPAT_INCLUDE_DIR SKYPAT_LIBRARIES)
