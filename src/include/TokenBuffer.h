014c8212c6ee286905c03f14ea43fdc52318a423
LuisHsu <luishsuplus@gmail.com>
2018-08-25 19:25:25 +0800

    實作 TokenBuffer

---
+++ src/include/TokenBuffer.h
@@ -0,0 +1,16 @@
+#ifndef WVMCC_TOKENBUFFER_DEF
+#define WVMCC_TOKENBUFFER_DEF
+
+#include <stdlib.h>
+#include <adt/list.h>
+#include <adt/Token.h>
+#include <adt/Buffer.h>
+
+typedef struct _TokenBuffer{
+    Buffer buffer;
+    void (*add)(struct _TokenBuffer* tokenBuffer, Token* token);
+    void (*free)(struct _TokenBuffer** tokenBuffer);
+} TokenBuffer;
+
+TokenBuffer* new_TokenBuffer();
+#endif
---
+++ test/unittests/TokenBuffer_unittest.hpp
@@ -0,0 +1,34 @@
+#include <skypat/skypat.h>
+
+#define restrict __restrict__
+extern "C"{
+    #include <TokenBuffer.h>
+    #include <string.h>
+}
+#undef restrict
+
+SKYPAT_F(TokenBuffer_unittest, create_delete)
+{
+    TokenBuffer* tokenBuffer = new_TokenBuffer();
+    EXPECT_EQ(tokenBuffer->buffer.length, 0);
+    tokenBuffer->free(&tokenBuffer);
+    EXPECT_EQ(tokenBuffer, NULL);
+}
+
+SKYPAT_F(TokenBuffer_unittest, polymorphism)
+{
+    Buffer* buffer = (Buffer*)new_TokenBuffer();
+    EXPECT_EQ(buffer->length, 0);
+    buffer->free(&buffer);
+    EXPECT_EQ(buffer, NULL);
+}
+
+SKYPAT_F(TokenBuffer_unittest, add_token)
+{
+    TokenBuffer* tokenBuffer = (TokenBuffer*)new_TokenBuffer();
+    Token* token = new_UnknownToken();
+    tokenBuffer->add(tokenBuffer, token);
+    EXPECT_EQ(tokenBuffer->buffer.length, 1);
+    EXPECT_EQ(((List*)(tokenBuffer->buffer.data))->size, 1);
+    tokenBuffer->free(&tokenBuffer);
+}
