66f6608ed8d69c911acf7dc08bbd3ab83907c4c1
LuisHsu <luishsuplus@gmail.com>
2018-09-06 18:34:54 +0800

    修改 adt 底下的檔名大小寫

---
+++ src/include/adt/Stack.h
@@ -0,0 +1,23 @@
+#ifndef WVMCC_STACK_DEF
+#define WVMCC_STACK_DEF
+
+#include <stdlib.h>
+
+typedef struct StackNode_ {
+  void* data;
+  struct StackNode_* next;
+} StackNode;
+
+typedef struct {
+  StackNode* head;
+  unsigned int size;
+} Stack;
+
+Stack* stackNew();
+void stackPush(Stack* thisStack, void* data);
+// If success return 0, or return -1
+int stackPop(Stack* thisStack, void** dataPtr);
+int stackTop(Stack* thisStack, void** dataPtr);
+void stackFree(Stack** thisStackPtr);
+
+#endif  // !WVMCC_STACK_DEF
